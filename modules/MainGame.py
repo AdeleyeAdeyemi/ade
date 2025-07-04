8dd0973463198d90a1d4663e8af58544cee945b8
Undefinedboss <josekadel355@gmail.com>
2025-07-04 22:26:58 +0100

    Update MainGame.py

--- modules/MainGame.py
+++ modules/MainGame.py
@@ -1,5 +1,5 @@
 from modules.live import welcome_message, print_game_options
-from Currency_Roulette import Currency_Roulette_Game
+from Currency_Roulette import Currency_Roulette
 from Guess_Game import Guess_Game
 from Memory_Game import Memory_Game
 
