6d78ef9f0778232b5d9425dfa6ed2b20bbd5be76
Undefinedboss <josekadel355@gmail.com>
2025-07-04 21:19:34 +0100

    Update live.py

--- modules/live.py
+++ modules/live.py
@@ -1,6 +1,6 @@
 from modules.Memory_Game import Memory_Game
-from modules.Guess_Game import play as play_guess
-from modules.Currency_Roulette import play as play_currency
+from modules.Guess_Game import Guess_Game
+from modules.Currency_Roulette import Currency_Roulette
 
 
 def run_memory_game(level):
