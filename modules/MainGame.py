1d5f192ca731e044bba60173c9956f4544ac35f6
Undefinedboss <josekadel355@gmail.com>
2025-07-04 22:23:34 +0100

    Update MainGame.py

--- modules/MainGame.py
+++ modules/MainGame.py
@@ -1,8 +1,8 @@
-from modules.live import welcome_message, print_game_option
+from modules.live import welcome_message, print_game_options
 from Currency_Roulette import Currency_Roulette_Game
 from Guess_Game import Guess_Game
 from Memory_Game import Memory_Game
 
 name = input("Insert your name: ")
 print(welcome_message(name))
-print_game_option()
+print_game_options(name)
