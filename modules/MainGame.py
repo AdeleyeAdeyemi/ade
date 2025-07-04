838ad9e7eb1e648086bcc874935886e09203bae2
Undefinedboss <josekadel355@gmail.com>
2025-07-04 22:47:55 +0100

    Update MainGame.py

--- modules/MainGame.py
+++ modules/MainGame.py
@@ -1,8 +1,17 @@
-from modules.live import welcome_message, print_game_options
-from .Currency_Roulette import play as currency_roulette_game
-from .Guess_Game import Guess_Game
-from .Memory_Game import Memory_Game
+import os
 
-name = input("Insert your name: ")
-print(welcome_message(name))
-print_game_options(name)
+if __name__ == "__main__":
+    env = os.environ.get("ENV", "dev")
+
+    if env == "dev":
+        name = input("Insert your name: ")
+    else:
+        name = os.environ.get("PLAYER_NAME", "Player")
+
+    from modules.live import welcome_message, print_game_options
+    from modules.Currency_Roulette import play as currency_roulette_game
+    from modules.Guess_Game import Guess_Game
+    from modules.Memory_Game import Memory_Game
+
+    print(welcome_message(name))
+    print_game_options(name)
